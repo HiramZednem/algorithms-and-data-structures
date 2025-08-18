4d113363c5ba2944223627669e7f65c38dde9d7e
Hiram Méndez <hirammendez000@gmail.com>
2025-08-18 09:43:49 -0600

    hashing

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
@@ -1,14 +1,46 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_unique;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /*
  * Dado un método que recibe una String, comprobar si todos los caracteres son únicos o no.
  *
  * isUnique("abcde") => true;
  * isUnique("abcded") => false;
+ *
+ * Mi implementacion no es mala. Tiene una complejidad aparente de O(n)....
+ * O bueno, eso es lo que pensaba. Yo creia que estaba evitando ese n^2
+ * al usar mi 'ArrayList'.
+ *
+ * Pero un ArrayList a la hora de agregar un nuevo registro, como no apunta a direcciones de memoria y tiene un indice
+ * fijo, se vuelve pesado. Ademas la consulta pasa un poco mas de lo mismo. ese contains empieza a buscar dentro de y
+ * eso vuelve mi codigo a que no sea tan perro.
+ *
+ * En cambio si creo un HashList. Este codigo seria mejor. Porque para buscar, hasheo el index y ya tengo la posicion a
+ * donde ir a buscar. Y el agregar, simplemente adentro tiene una lista enlazada
+ *
+ *
+ * Eso como primera optimizacion. Lo bajo a O(n)...
+ *
+ * Sin embargo, esto se puede reducir aun mas.
+ * Un alfabeto de caracteres ascii contiene 128 chars. por tanto
+ * si contiene mas mi string es obvio que se duplican chars. por tanto el codigo quedaria asi
  */
 public class IsUnique {
 
+  private static final int NUMBER_CHARS = 128;
   public boolean isUnique(String s) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    if( s.length() > NUMBER_CHARS ) return false;
+
+    Set<Character> characters = new HashSet<>();
+    for(Character c : s.toCharArray()) {
+      if (characters.contains(c)) return false;
+      characters.add(c);
+    }
+    return true;
   }
 }
--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_02_two_sum/TwoSum.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_02_two_sum/TwoSum.java
@@ -1,5 +1,12 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._02_two_sum;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /*
  * Dado un array de números enteros y un target, retorna los índices de dos
  * números para los que la suma de ambos sea igual al target.
@@ -18,6 +25,41 @@ package com.danielblanco.algoritmosestructuras.arraysstringshashtables._02_two_s
 public class TwoSum {
 
   public int[] twoSum(int[] nums, int target) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    /**
+     * La primera vez que resolvi este ejercicio, lo hice con dos ciclos for, aun me acuerdo xd
+     * pero ahora soy alguien diferente...
+     *
+     * A ver tengo una solucion bien peraa, pensaba implementar otra vez mi arraylist de mrd, pero
+     * para busqueda esta del culo...
+     *
+     * Por lo que lo que voy a hacer es crear un hashlist para insertar todos los valores.
+     *
+     * Y despues voy a recorrer el hash list uno por uno y voy a buscar el faltante en el hash
+     */
+
+    // Me rendi pq no pude sacar el indice de un hash, pero salaverga esta solucion...
+//    List<Integer> numbers = new ArrayList<>();
+//
+//    for (int n : nums ) {
+//      numbers.add(n);
+//    }
+//
+//    for (int n: numbers) {
+//      if(numbers.contains( target - n )) {
+//        return new int[]{numbers.indexOf(n), numbers.indexOf(target-n)};
+//      }
+//    }
+
+//    return null;
+
+
+    Map<Integer, Integer> compMap = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+
+      if( compMap.containsKey( nums[i] ) ) return new int[]{ compMap.get(nums[i]) , i};
+      compMap.put(target-nums[i], i);
+    }
+    return null;
   }
 }
