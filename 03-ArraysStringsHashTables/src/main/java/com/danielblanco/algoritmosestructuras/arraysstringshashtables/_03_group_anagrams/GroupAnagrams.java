702b8b057e08474a13760029602809dc6509aaa8
Hiram Méndez <hirammendez000@gmail.com>
2025-08-19 10:23:27 -0600

    [ADD]: GroupAnagrams

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagrams.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagrams.java
@@ -1,5 +1,8 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._03_group_anagrams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /*
@@ -12,7 +15,68 @@ import java.util.List;
  */
 public class GroupAnagrams {
 
-  public List<List<String>> groupAnagrams(String[] words) {
-    throw new UnsupportedOperationException("Not implemented yet");
+  public static List<List<String>> groupAnagrams(String[] words) {
+    /**
+     * Mi solucion esta en mi cabeza pero a la vez no!!!
+     *
+     * La primera evidentemente es si contiene las mismas letras.
+     * Caracteres.
+     *
+     * - Recorrer la primera del arreglo.
+     * - Obtener la segunda del arreglo
+     * - validar si s esta en la segunda palabra, si a esta en la segunda palabra, C...
+     * -> Muy complejo O(n^3)
+     * ----
+     *
+     * -> Verificar la longitud de la palabra.
+     * -> Si existe una con la misma longitud
+     *
+     *
+     * Verga a ver se me ocurre esa del hash, el pedo es que adentro tiene que ir una lista xd, entonces la complejidad
+     * sigue elevando, y sumanod que lo quiero ordenar antes de meter
+     * Primero se me ocurre en un hash agregar la longitud y luego la palabra
+     */
+
+
+    /**
+     * Espera mi cerebro acaba de dar la solucion
+     *
+     * Llega y la ordeno y la guardo
+     * Llave (palabra ordenada), key: palabra normal
+     *
+     * if contains (palabra ordenada)
+     * results push(new String{hash.get(palabraOrdenada), palabra})
+     */
+
+    if (words.length == 0) {
+      return Arrays.asList(Arrays.asList());
+    }
+
+    HashMap<String, List<String>> sortedStrings = new HashMap<>();
+    List<List<String>> results = new ArrayList<>();
+
+    for(int i = 0; i < words.length; i++) {
+      String word = words[i];
+      char[] chars = word.toCharArray();
+      Arrays.sort(chars);
+      String sortedWord = new String(chars);
+
+      if (!sortedStrings.containsKey(sortedWord)) {
+        sortedStrings.put(sortedWord, new ArrayList<>());
+      }
+
+      sortedStrings.get(sortedWord).add(word);
+    }
+
+    for (String key: sortedStrings.keySet()) {
+      results.add(sortedStrings.get(key));
+    }
+
+    System.out.println(sortedStrings.values().toString());
+    return new ArrayList<>(sortedStrings.values());
+  }
+
+  public static void main(String[] args) {
+    groupAnagrams(new String[] {"saco", "arresto", "programa", "rastreo", "caso"});
   }
 }
